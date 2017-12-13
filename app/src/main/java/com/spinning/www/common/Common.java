462f970e04556ec95b0ac32a463ba7f379b3b70a
361513999 <361513999@163.com>
2017-12-13 18:42:56 +0800

    增加部分基础工具类

--- app/src/main/java/com/spinning/www/common/Common.java
+++ app/src/main/java/com/spinning/www/common/Common.java
@@ -7,6 +7,7 @@ import android.os.Environment;
  */
 
 public class Common {
+
     public static final String config = "CONFIG";
     public static String BASE_DIR = Environment
             .getExternalStorageDirectory().getAbsolutePath();
