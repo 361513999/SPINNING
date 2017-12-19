2b404e2d79d16b318515c611b6699b643b1f2c4d
cloor <351513999@163.com>
2017-12-20 00:07:12 +0800

    新增部分

---
+++ app/src/main/java/com/hhkj/spinning/www/ui/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.hhkj.spinning.www.ui;
+
+import android.os.Bundle;
+
+import com.hhkj.spinning.www.R;
+import com.hhkj.spinning.www.base.BaseActivity;
+
+/**
+ * Created by cloor on 2017/12/19.
+ */
+
+public class HomeActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_layout);
+    }
+
+    @Override
+    public void process(int what) {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
