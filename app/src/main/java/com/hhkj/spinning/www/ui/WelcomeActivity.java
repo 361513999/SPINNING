7af37fcf70f3f1bc3c72ebe7c1b1d0ffb74e347f
361513999 <361513999@163.com>
2018-01-25 16:29:09 +0800

    修复部分列表问题和直播状态调整

--- app/src/main/java/com/hhkj/spinning/www/ui/WelcomeActivity.java
+++ app/src/main/java/com/hhkj/spinning/www/ui/WelcomeActivity.java
@@ -4,31 +4,130 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Message;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
 
+import com.alivc.player.AliVcMediaPlayer;
+import com.alivc.player.MediaPlayer;
 import com.hhkj.spinning.www.R;
 import com.hhkj.spinning.www.base.AppManager;
 import com.hhkj.spinning.www.base.BaseActivity;
-import com.hhkj.spinning.www.bean.PersonCenter0;
-import com.hhkj.spinning.www.common.TimeUtil;
+import com.hhkj.spinning.www.common.Common;
+import com.hhkj.spinning.www.common.P;
 import com.hhkj.spinning.www.inter.Result;
+import com.hhkj.spinning.www.media.NEVideoView;
+import com.hhkj.spinning.www.utils.FileUtils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
 
 /**
  * Created by Administrator on 2017/12/14/014.
  */
 
 public class WelcomeActivity extends BaseActivity {
+    private SurfaceView suf;
+    private AliVcMediaPlayer mediaPlayer;
+    private TextView enter;
+
+
+
+
+
+
     @Override
     public void init() {
-        time();
+
+
+       // time();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mediaPlayer!=null){
+            mediaPlayer.pause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer!=null){
+            mediaPlayer.destroy();
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_layout);
+
+        suf = findViewById(R.id.suf);
+        mediaPlayer = new AliVcMediaPlayer(WelcomeActivity.this,suf);
+        suf.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if(mediaPlayer!=null){
+                    mediaPlayer.setVideoSurface(surfaceHolder.getSurface());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.setSurfaceChanged();
+                }
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+            }
+        });
+        File file = new File(Common.SD+ "welcome.flv");
+        if(file.exists()){
+            getHandler().sendEmptyMessage(1);
+        }else{
+            showLoadView();
+            FileUtils.getInstance(WelcomeActivity.this).copyAssetsToSD("film",Common.DIR).setFileOperateCallback(new FileUtils.FileOperateCallback() {
+                @Override
+                public void onSuccess() {
+                    getHandler().sendEmptyMessage(1);
+                    P.c("复制成功");
+                }
+
+                @Override
+                public void onFailed(String error) {
+                    P.c("复制失败"+error);
+                }
+            });
+        }
+
+        mediaPlayer.setCompletedListener(new MediaPlayer.MediaPlayerCompletedListener() {
+            @Override
+            public void onCompleted() {
+                getHandler().sendEmptyMessage(0);
+            }
+        });
+        enter = findViewById(R.id.enter);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer!=null){
+                    mediaPlayer.pause();
+                    mediaPlayer.reset();
+                }
+                getHandler().sendEmptyMessage(0);
+            }
+        });
+
+
         load();
     }
     private void load(){
@@ -74,9 +173,9 @@ public class WelcomeActivity extends BaseActivity {
                 if(countDownTimer!=null){
                     countDownTimer.cancel();
                 }
-                Intent intent = new Intent(WelcomeActivity.this,LoginActivity.class);
+               /* Intent intent = new Intent(WelcomeActivity.this,LoginActivity.class);
                 startActivity(intent);
-                AppManager.getAppManager().finishAllActivity();
+                AppManager.getAppManager().finishAllActivity();*/
             }
         },false);
     }
@@ -95,6 +194,14 @@ public class WelcomeActivity extends BaseActivity {
                }
                 startActivity(intent);
                 AppManager.getAppManager().finishActivity();
+                break;
+            case 1:
+                cancleLoadView();
+                if (mediaPlayer != null) {
+                    mediaPlayer.prepareToPlay(Common.SD+ "welcome.flv");
+                    // mediaPlayer.prepareAndPlay();
+                }
+                mediaPlayer.play();
                 break;
         }
     }
